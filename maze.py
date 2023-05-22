8b9a8a4232e647bb8a184a4fa1198038dcfe96d1
jednadva <josef.stach@gjkt.eu>
2023-05-22 14:39:30 +0200

    Canvas

---
+++ maze.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+hlavni = TK()
+
+w = Canvas(hlavni, width=50, height=506)
+w.pack()
+
+a = 5
+b = 5
+
+for x in range(4):
+    w.create_line()
+class  Maze:
+    def __init__(self):
+
+
+class Robot:
+
+
+
+
+mainloop()
