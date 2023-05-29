1b5a016dde9d406fc66310dd08840508b85c8dbe
jednadva <josef.stach@gjkt.eu>
2023-05-29 14:22:22 +0200

    zalozeni

--- maze.py
+++ maze.py
@@ -1,15 +1,17 @@
 from tkinter import *
 
-hlavni = TK()
+hlavni = Tk()
 
-w = Canvas(hlavni, width=50, height=506)
+w = Canvas(hlavni, width=506, height=506)
 w.pack()
 
 a = 5
 b = 5
 
-for x in range(4):
-    w.create_line()
+for x in range(11):
+    w.create_line(5,a,505,b)
+    a += 50
+    b += 50
 class  Maze:
     def __init__(self):
 
