595a5bdc29ad46579de1c8d36474deb3c3ab7001
jednadva <josef.stach@gjkt.eu>
2023-05-29 14:33:52 +0200

    zacatek

--- okno.py
+++ okno.py
@@ -5,9 +5,12 @@ hlavni = Tk()
 w = Canvas(hlavni, width=1080, height=720)
 w.pack()
 
-b = Button(hlavni, text=u"START", activebackground="red", background="green", font= (None, 30), height=10, width=10)
-b.place(x = 500, y = 500)
-b.pack()
+startbtn = Button(hlavni, text=u"START", activebackground="red", background="green", font= (None, 30), height=10, width=10)
+startbtn.place(x = 500, y = 500)
+startbtn.pack()
+
+easyoption = Button(hlavni,text=u"EASY",activebackground="yellow", font= (None, 30), height=10, width=10 )
+easyoption.pack()
 
 
 mainloop()
